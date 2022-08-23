303be489af8a20b6102c2509f80a2c0852713f7b
hatim77100 <najoua-hatim@hotmail.fr>
2022-08-23 16:54:22 +0200

    header start

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -13,12 +14,17 @@ module.exports = {
       template: path.resolve(__dirname, "./src/index.html"),
       filename: "index.html",
     }),
+    new CopyPlugin({
+      patterns: [
+        { from: "./src/assets/img/", to: "img" }
+      ],
+    }),
   ],
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
-      clean: true,
-      publicPath: '/',
+    clean: true,
+    publicPath: "/",
   },
   optimization: {
     runtimeChunk: "single",
