f0d9d0f216112f75e0514c422fa198f212272064
hatim77100 <najoua-hatim@hotmail.fr>
2022-09-02 16:57:14 +0200

    update mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+      'sm': '320px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {},
   },
   plugins: [],
