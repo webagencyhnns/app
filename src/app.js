1534942ad09d5f717a3a3bfc01041a438ca452a4
hatim77100 <najoua-hatim@hotmail.fr>
2022-10-03 16:22:51 +0200

    update project

--- src/app.js
+++ src/app.js
@@ -1,32 +1,17 @@
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
+AOS.init();
 import '@fortawesome/fontawesome-free/js/all'
 if (process.env.NODE_ENV !== 'production') {
     console.log('Looks like we are in development mode!');
   }
 
-// import _ from 'lodash';
-// import './style.css';
-// import Icon from './icon.png/opti.png';
-
-// function component() {
-//     const element = document.createElement('div');
-
-//     // Lodash, currently included via a script, is required for this line to work
-//     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-//     element.classList.add('hello');
-//      // Add the image to our existing div.
-//   const myIcon = new Image();
-//   myIcon.src = Icon;
-
-//   element.appendChild(myIcon);
-//     return element;
-//   }
-
-//   document.body.appendChild(component());
-
 const menu = document.getElementById("menu");
 const liste = document.getElementById('liste');
 menu.addEventListener("click", () => {
-   liste.classList.toggle('hidden');
+    liste.classList.toggle('hidden');
+   
     // p.classList.toggle('color');
 })
 
@@ -34,9 +19,6 @@ const allLinks = document.querySelectorAll('.li-nav');
 
 allLinks.forEach(link => {
     link.addEventListener("click", () => {
-        liste.classList.toggle('hidden'); 
+        liste.classList.toggle('hidden');
     })
 })
-// console.log(menu, liste);
-// import Menu from './component/menu';
-// Menu();
